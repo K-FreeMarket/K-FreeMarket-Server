00847b98a657b81c083aad646c2233645ca18dbb
wjdsheep15 <wjd15sheep@gmail.com>
2025-06-09 02:38:05 +0900

    Feat: init

---
+++ src/main/java/com/kfreemarket/reemarket_server/ReemarketServerApplication.java
@@ -0,0 +1,13 @@
+package com.kfreemarket.reemarket_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReemarketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReemarketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kfreemarket/reemarket_server/ReemarketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfreemarket.reemarket_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReemarketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
