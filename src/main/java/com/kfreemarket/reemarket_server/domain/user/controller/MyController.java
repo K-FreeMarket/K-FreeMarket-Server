d83a524771ea0ce46219610aacc43606f0f9fe62
wjdsheep15 <wjd15sheep@gmail.com>
2025-07-06 16:54:33 +0900

    Feat: Add thymeleaf

--- src/main/java/com/kfreemarket/reemarket_server/domain/user/controller/MyController.java
+++ src/main/java/com/kfreemarket/reemarket_server/domain/user/controller/MyController.java
@@ -1,15 +1,17 @@
 package com.kfreemarket.reemarket_server.domain.user.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
 
-    @GetMapping("/my")
-    @ResponseBody
-    public String myAPI() {
-        return "my route";
+    @RequestMapping(value="/admin",method = RequestMethod.GET)
+    public String hello(Model model){
+    model.addAttribute("hello","서버입니다");
+    return "/hello";
     }
+
 }
