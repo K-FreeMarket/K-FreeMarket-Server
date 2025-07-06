1cd9ce8ceaa9133d77ec7e01e303362d1d8d5d88
wjdsheep15 <wjd15sheep@gmail.com>
2025-07-06 22:09:40 +0900

    Fix: SecurityConfig /api/products/** permitAll()

--- src/main/java/com/kfreemarket/reemarket_server/global/config/SecurityConfig.java
+++ src/main/java/com/kfreemarket/reemarket_server/global/config/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers( "/reissue/**", "/auth/token").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
-                        .requestMatchers("/admin/**", "/api/**").permitAll()
+                        .requestMatchers("/admin/**", "/api/products/**").permitAll()
                         .anyRequest().authenticated());
 
         http
