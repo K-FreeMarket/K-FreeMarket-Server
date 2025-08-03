bafbcb528bac477f6820440b0c6e56e020387418
wjdsheep15 <wjd15sheep@gmail.com>
2025-08-03 16:11:16 +0900

    Fix: application env value

--- src/main/java/com/kfreemarket/reemarket_server/ReemarketServerApplication.java
+++ src/main/java/com/kfreemarket/reemarket_server/ReemarketServerApplication.java
@@ -2,10 +2,15 @@ package com.kfreemarket.reemarket_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:properties/env.properties") // env.properties 파일 소스 등록
+})
 public class ReemarketServerApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/kfreemarket/reemarket_server/ReemarketServerApplicationTests.java
+++ src/test/java/com/kfreemarket/reemarket_server/ReemarketServerApplicationTests.java
@@ -7,9 +7,7 @@ import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles("test")
 @SpringBootTest
-@TestPropertySource(properties = {
-		"jwt.secretKey=3488e49ce4419434d1ac6566e4841d210a9f3d58676f35188989e0b83dba26636a5fb138a636142697b4c59309b27d122784f144ab7efee9521a6b78fa879eae"
-})
+@TestPropertySource("classpath:properties/env.properties")
 class ReemarketServerApplicationTests {
 
 	@Test
